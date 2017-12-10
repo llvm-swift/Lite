b602ad9845119054c8fe9a58c6824352f23e6f79
Harlan Haskins <harlan@harlanhaskins.com>
2017-12-10 13:31:09 -0500

    Use tagged revision for ShellOut

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.0.0"),
-    .package(url: "https://github.com/harlanhaskins/ShellOut.git", .branch("on-your-mark-get-set-go")),
+    .package(url: "https://github.com/harlanhaskins/ShellOut.git", from: "2.0.2"),
   ],
   targets: [
     .target(
