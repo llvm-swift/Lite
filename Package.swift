79351cf5f6bba8ab9744785c8558e180a231d063
Max Desiatov <max@desiatov.com>
2021-11-21 22:40:06 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.5"),
-    .package(url: "https://github.com/apple/swift-tools-support-core.git", .upToNextMinor(from: "0.2.3"))
+    .package(url: "https://github.com/apple/swift-tools-support-core.git", .upToNextMinor(from: "0.2.4"))
   ],
   targets: [
     .target(
